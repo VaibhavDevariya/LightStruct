501c051a25682638f3bfce088ed3bed8a7854f92
Vaibhav Devariya <vaibhav.dev@outlook.com>
2025-05-05 21:14:01 +0530

    Formatted and added operator[] to access the key

--- LightStruct.h
+++ LightStruct.h
@@ -31,8 +31,7 @@ public:
     LightStruct(const Array& arr) : value_(arr) {}
     LightStruct(const Object& obj) : value_(obj) {}
     
-    static LightStruct fromFile(const std::string& filename) 
-    {
+    static LightStruct fromFile(const std::string& filename) {
         std::ifstream in(filename);
         if (!in) throw std::runtime_error("Failed to open file");
     
@@ -46,11 +45,11 @@ public:
     }
 
     bool getBool(const std::string& key) const {
-        if (auto obj = std::get_if<Object>(&value_))
-        {
+        if (auto obj = std::get_if<Object>(&value_)) {
             if (auto pval = std::get_if<bool>(&obj->at(key).value_))
                 return *pval;
         }
+        throw std::runtime_error("Value is not a bool for: " + key);
     }
 
     int getInt(const std::string& key) const {
@@ -62,54 +61,62 @@ public:
     }
 
     double getDouble(const std::string& key) const {
-        if(auto obj = std::get_if<Object>(&value_))
-            if (auto pval = std::get_if<double>(&obj->at(key).value_)) 
+        if (auto obj = std::get_if<Object>(&value_)) {
+            if (auto pval = std::get_if<double>(&obj->at(key).value_))
                 return *pval;
+        }
         throw std::runtime_error("Value is not a double for: " + key);
     }
     
     std::string getString(const std::string& key) const {
-        if(auto obj = std::get_if<Object>(&value_))
-            if (auto pval = std::get_if<std::string>(&obj->at(key).value_)) return *pval;
+        if (auto obj = std::get_if<Object>(&value_)) {
+            if (auto pval = std::get_if<std::string>(&obj->at(key).value_)) 
+                return *pval;
+        }
         throw std::runtime_error("Value is not a string for: " + key);
     }
     
     Array getArray(const std::string& key) const {
-        if (auto obj = std::get_if<Object>(&value_))
-            if (auto pval = std::get_if<Array>(&obj->at(key).value_)) return *pval;
+        if (auto obj = std::get_if<Object>(&value_)) {
+            if (auto pval = std::get_if<Array>(&obj->at(key).value_))
+                return *pval;
+        }
         throw std::runtime_error("Value is not an array for: " + key);
     }
 
     Object getObject(const std::string& key) const {
-        if (auto obj = std::get_if<Object>(&value_))
-            if (auto pval = std::get_if<Object>(&obj->at(key).value_)) return *pval;
+        if (auto obj = std::get_if<Object>(&value_)) {
+            if (auto pval = std::get_if<Object>(&obj->at(key).value_))
+                return *pval;
+        }
         throw std::runtime_error("Value is not an object for: " + key);
+    }
+
+    LightStruct& operator[](const std::string &key) {
+        return std::get<Object>(value_).at(key);
     }
 
 private:
 
 // ---------------------- PARSING
-    void parse()
-    {
+    void parse() {
         skipWhitespaces();
         value_ = parseObject().value(); 
     }
 
-    void skipWhitespaces()
-    {
-        while (pos_ < text_.size() && std::isspace(text_[pos_])) ++pos_;
+    void skipWhitespaces() {
+        while(pos_ < text_.size() && std::isspace(text_[pos_])) 
+            ++pos_;
     }
 
-    std::optional<Object> parseObject()
-    {
+    std::optional<Object> parseObject() {
         if (text_[pos_] != '{') return std::nullopt;
         ++pos_;
         skipWhitespaces();
 
         Object obj;
 
-        while (pos_ < text_.size()) 
-        {
+        while(pos_ < text_.size()) {
             if (text_[pos_] == '}') { ++pos_; break; }
 
             std::string key = parseString().value();
@@ -139,14 +146,12 @@ private:
         return obj;
     }
 
-    std::optional<std::string> parseString()
-    {
+    std::optional<std::string> parseString() {
         if (text_[pos_] != '"') return std::nullopt;
         ++pos_;
         std::ostringstream oss;
 
-        while (pos_ < text_.size())
-        {
+        while (pos_ < text_.size()) {
             char c = text_[pos_++];
             if (c == '"') break;
             if (c == '\\') {
@@ -156,15 +161,15 @@ private:
                     else if (next == '\\') oss << '\\';
                     else throw std::runtime_error("Unsupported escape sequence");
                 }
-            } else {
+            } 
+            else {
                 oss << c;
             }
         }
         return oss.str();
     }
 
-    std::optional<LightStruct> parseValue()
-    {
+    std::optional<LightStruct> parseValue() {
         if (pos_ >= text_.size()) return std::nullopt;
 
         if (text_[pos_] == '"') {
@@ -178,22 +183,20 @@ private:
         else if (text_[pos_] == '[') {
             Array arr = parseArray().value();
             return LightStruct{arr};
-        } else 
-        {
+        } 
+        else {
             return parseLiteral();
         }
     }
 
-    std::optional<Array> parseArray()
-    {
+    std::optional<Array> parseArray() {
         if (text_[pos_] != '[') return std::nullopt;
         ++pos_;
         skipWhitespaces();
 
         Array arr;
 
-        while (pos_ < text_.size()) 
-        {
+        while (pos_ < text_.size()) {
             if (text_[pos_] == ']') { ++pos_; break; }
 
             arr.emplace_back(std::move(parseValue().value()));
@@ -201,18 +204,19 @@ private:
             if (text_[pos_] == ',') {
                 ++pos_;
                 skipWhitespaces();
-            } else if (text_[pos_] == ']') {
+            } 
+            else if (text_[pos_] == ']') {
                 ++pos_;
                 break;
-            } else {
+            } 
+            else {
                 throw std::runtime_error("Expected ',' or ']' in array");
             }
         }
         return arr;
     }
 
-    std::optional<LightStruct> parseLiteral()
-    {
+    std::optional<LightStruct> parseLiteral() {
         size_t start = pos_;
         while (pos_ < text_.size() && (isalnum(text_[pos_]) || text_[pos_] == '.' || text_[pos_] == '-'))
             ++pos_;
@@ -227,13 +231,13 @@ private:
                 return LightStruct(std::stod(token));
             else
                 return LightStruct(std::stoi(token));
-        } catch (...) {
+        } 
+        catch (...) {
             throw std::runtime_error("Invalid literal: " + token);
         }
     }
 
-    inline std::string escapeString(const std::string& s) 
-    {
+    inline std::string escapeString(const std::string& s) {
         std::ostringstream oss;
         oss << '"';
         for (char c : s) {
@@ -244,14 +248,4 @@ private:
         oss << '"';
         return oss.str();
     }
-
 };
-    // const Object& asObject() const { return std::get<Object>(value_); }
-    // const Array& asArray() const { return std::get<Array>(value_); }
-    // const std::string& asString() const { return std::get<std::string>(value_); }
-    // double asNumber() const { return std::get<double>(value_); }
-    // bool asBool() const { return std::get<bool>(value_); }
-
-    // Object& asObject() { return std::get<Object>(value_); }
-    // Array& asArray() { return std::get<Array>(value_); }
-// };
